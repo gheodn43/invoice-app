5c54758629c98302ece3f34d7f525c62fa541439
gheodn43 <97235672+gheodn43@users.noreply.github.com>
2023-12-05 08:29:44 -0600

    init node project

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+  return (
+    <div className="App">
+      <p>{!data ? "Loading..." : data}</p>
+    </div>
+  );
+}
+
+export default App;
